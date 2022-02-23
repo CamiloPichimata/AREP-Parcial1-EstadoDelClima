9e1cebc07a370f73b6101a0ad834d0e18e0df8c0
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-02-23 17:22:17 -0500

    Implementación página

--- src/main/java/edu/eci/arep/estadoClima/HttpServer.java
+++ src/main/java/edu/eci/arep/estadoClima/HttpServer.java
@@ -19,7 +19,6 @@ public class HttpServer {
 		
 		while (running) {
 		Socket clientSocket = null;
-		
 		try {
 			System.out.println("Listo para recibir ...");
 			clientSocket = serverSocket.accept();
@@ -33,31 +32,67 @@ public class HttpServer {
 		new InputStreamReader(clientSocket.getInputStream()));
 		String inputLine, outputLine;
 		
+		boolean primeraLinea = true;
+		String file = "";
 		while ((inputLine = in.readLine()) != null) {
-			System.out.println("Received: " + inputLine);
-			if (!in.ready()) {
-				break;
-			}
-		}
-			outputLine = "HTTP/1.1 200 OK\n\r"
+            System.out.println("Received: " + inputLine);
+            if (primeraLinea) {
+                file = inputLine.split(" ")[1];
+                System.out.println("File: " + file);
+                primeraLinea = false;
+            }
+            if (!in.ready()) {
+                break;
+            }
+        }
+		if (file.startsWith("/Clima")) {
+        	outputLine = "HTTP/1.1 200 OK\r\n"
                     + "Content-Type: text/html\r\n"
                     + "\r\n"
                     + "<!DOCTYPE html>"
                     + "<html>"
                     + "<head>"
                     + "<meta charset=\"UTF-8\">"
-                    + "<title>Title of the document</title>\n"
+                    + "<title>Estado del Clima</title>\n"
                     + "</head>"
                     + "<body>"
-                    + "My Web Site"
+                    + "<h1>Estado del clima</h1>"
+                    + "<form class='Form'>"
+                    + "<label for='Get-Clima'>Lugar a consultar: </label>"
+                    + "<input id='Get-Clima' type='text'>"
+                    + "<input type='button' value='Consultar' class='Boton' onclick=clickF()>"
+                    + "</body>"
+                    + "<script>"
+                    + "var boton = document.getElementById('Boton');"
+                    + "var clickF = function() {console.log('Click en el boton de consulta');}"
+                    + "</script>"
+                    + "</html>";
+        
+        } else if (file.startsWith("/Consulta")) {
+        	outputLine = "No implementado aun";
+        	// Realizar la consulta al API
+        	
+    	}else {
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n"
+                    + "<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<meta charset=\"UTF-8\">"
+                    + "<title>Consulta erronea</title>\n"
+                    + "</head>"
+                    + "<body>"
+                    + "<h2>La URL ingresada no es correcta</h2>"
                     + "</body>"
                     + "</html>";
+        }
 			out.println(outputLine);
 			out.close();
 			in.close();
 			clientSocket.close();
-			serverSocket.close();
 		}
+		serverSocket.close();
 	}
 	
 	static int getPort() {
